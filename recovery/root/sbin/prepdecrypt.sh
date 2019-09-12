20c2e29fcbf80136019ce22d9ca4d9278d9f7eb3
C457 <android.c357@gmail.com>
2019-09-12 18:26:08 -0500

    pine: Grab a log to /persist/ before we exit
    * I should probably use a counter to grab multiple logs each time grablog is called

--- recovery/root/sbin/prepdecrypt.sh
+++ recovery/root/sbin/prepdecrypt.sh
@@ -10,14 +10,24 @@ relink()
 
 finish()
 {
+	grablog
 	exit 0
 	umount /v
 	umount /s
+        umount /p
 	rmdir /v
 	rmdir /s
+        rmdir /p
 	setprop crypto.ready 1
 	setprop prep.decrypt 1
 	exit 0
+}
+
+grablog()
+{
+        mkdir -p /p/twrp/
+	cp /tmp/recovery.log /p/twrp/
+        dmesg > /p/twrp/dmesg_twrp.txt
 }
 
 suffix=$(getprop ro.boot.slot_suffix)
@@ -32,6 +42,10 @@ syspath="/dev/block/bootdevice/by-name/system_a"
 mkdir /s
 mount -t ext4 -o ro "$syspath" /s
 mount /s/system /system
+psyspath="dev/block/bootdevice/by-name/persist"
+mkdir /p
+mount -t ext4 -o rw "$psyspath" /p
+mount /p
 
 device_codename=$(getprop ro.boot.hardware)
 is_fastboot_twrp=$(getprop ro.boot.fastboot)
