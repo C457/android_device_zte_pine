1e5ab4d5f1e977fc3feed0dd63c6263791b04b22
C457 <android.c357@gmail.com>
2019-09-25 10:51:45 -0500

    pine: Build PowerHal service

--- device.mk
+++ device.mk
@@ -333,6 +333,9 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 # Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.2-service.pine
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
