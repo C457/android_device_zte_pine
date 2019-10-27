f36527ee7b30e79217d341fa2df3c8910e4341f6
C457 <android.c357@gmail.com>
2019-10-27 08:54:18 -0500

    fod: Add support for ZTE fingerprint extension

--- fod/FingerprintInscreen.h
+++ fod/FingerprintInscreen.h
@@ -18,6 +18,7 @@
 #define VENDOR_LINEAGE_BIOMETRICS_FINGERPRINT_INSCREEN_V1_0_FINGERPRINTINSCREEN_H
 
 #include <vendor/lineage/biometrics/fingerprint/inscreen/1.0/IFingerprintInscreen.h>
+#include <vendor/zte/hardware/fingerprintextension/1.0/IZteFingerprint.h>
 
 namespace vendor {
 namespace lineage {
@@ -30,6 +31,7 @@ namespace implementation {
 using ::android::sp;
 using ::android::hardware::Return;
 using ::android::hardware::Void;
+using ::vendor::zte::hardware::fingerprintextension::V1_0::IZteFingerprint;
 
 class FingerprintInscreen : public IFingerprintInscreen {
 public:
@@ -49,6 +51,9 @@ public:
     Return<int32_t> getDimAmount(int32_t cur_brightness) override;
     Return<bool> shouldBoostBrightness() override;
     Return<void> setCallback(const sp<::vendor::lineage::biometrics::fingerprint::inscreen::V1_0::IFingerprintInscreenCallback>& callback) override;
+
+private:
+    sp<IZteFingerprint> zteFingerprintService;
 };
 
 }  // namespace implementation
