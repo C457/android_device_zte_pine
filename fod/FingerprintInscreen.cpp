dc502479ed78b52eceac4abcd8d7f60db9556cc4
C457 <android.c357@gmail.com>
2019-10-27 03:43:49 -0500

    pine: Add initial lineage FOD HAL

---
+++ fod/FingerprintInscreen.cpp
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "FingerprintInscreenService"
+
+#include "FingerprintInscreen.h"
+
+#include <android-base/logging.h>
+#include <fstream>
+
+#define FOD_SENSOR_X 0
+#define FOD_SENSOR_Y 0
+#define FOD_SENSOR_SIZE 0
+
+namespace {
+
+template <typename T>
+static void set(const std::string& path, const T& value) {
+    std::ofstream file(path);
+    file << value;
+}
+
+} // anonymous namespace
+
+namespace vendor {
+namespace lineage {
+namespace biometrics {
+namespace fingerprint {
+namespace inscreen {
+namespace V1_0 {
+namespace implementation {
+
+FingerprintInscreen::FingerprintInscreen() {
+}
+
+Return<int32_t> FingerprintInscreen::getPositionX() {
+    return FOD_SENSOR_X;
+}
+
+Return<int32_t> FingerprintInscreen::getPositionY() {             
+    return FOD_SENSOR_Y;
+}
+
+Return<int32_t> FingerprintInscreen::getSize() {
+    return FOD_SENSOR_SIZE;
+}
+
+Return<void> FingerprintInscreen::onStartEnroll() {
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onFinishEnroll() {
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onPress() {
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onRelease() {
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onShowFODView() {
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onHideFODView() {
+    return Void();
+}
+
+Return<bool> FingerprintInscreen::handleAcquired(int32_t acquiredInfo, int32_t vendorCode) {
+    LOG(ERROR) << "acquiredInfo: " << acquiredInfo << ", vendorCode: " << vendorCode << "\n";
+    return false;
+}
+
+Return<bool> FingerprintInscreen::handleError(int32_t error, int32_t vendorCode) {
+    LOG(ERROR) << "error: " << error << ", vendorCode: " << vendorCode << "\n";
+    return false;
+}
+
+Return<void> FingerprintInscreen::setLongPressEnabled(bool) {
+    return Void();
+}
+
+Return<int32_t> FingerprintInscreen::getDimAmount(int32_t) {
+    return 0;
+}
+
+Return<bool> FingerprintInscreen::shouldBoostBrightness() {
+    return false;
+}
+
+Return<void> FingerprintInscreen::setCallback(const sp<::vendor::lineage::biometrics::fingerprint::inscreen::V1_0::IFingerprintInscreenCallback>& callback) {
+    (void) callback;
+    return Void();
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace inscreen
+}  // namespace fingerprint
+}  // namespace biometrics
+}  // namespace lineage
+}  // namespace vendor
